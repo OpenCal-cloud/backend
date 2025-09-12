326d7b51802728f011a1f39f2a96748f98b3e914
Anton Dachauer <ad@var-lab.com>
2025-09-12 12:45:01 +0200

    push changelog to documentation repository on release
    Changelog: chore

--- ci/push_changelog.sh
+++ ci/push_changelog.sh
@@ -2,6 +2,9 @@
 
 set -xe
 
+git config --global user.email "gitlab-ci@var-lab.com"
+git config --global user.name "GitLab CI"
+
 curl -H "PRIVATE-TOKEN: $CI_API_TOKEN" "$CI_API_V4_URL/projects/$CI_PROJECT_ID/repository/changelog?version=$CI_COMMIT_TAG" | jq -r .notes > release_notes.md
 git clone https://oauth2:$OPENCAL_PUSH_RELEASE_NOTES_ACCESS_TOKEN@git.var-lab.com/opencal/documentation.git documentation
 cd documentation
