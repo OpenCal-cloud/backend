99c812effe79b21d1a736ea9d997a4f7effc4b59
Anton Dachauer <ad@var-lab.com>
2025-09-12 12:17:17 +0200

    add git to pipeline job
    Changelog: chore
    
    # Conflicts:
    #	.gitlab-ci.yml

---
+++ ci/push_changelog.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -xe
+
+curl -H "PRIVATE-TOKEN: $CI_API_TOKEN" "$CI_API_V4_URL/projects/$CI_PROJECT_ID/repository/changelog?version=$CI_COMMIT_TAG" | jq -r .notes > release_notes.md
+git clone https://oauth2:$OPENCAL_PUSH_RELEASE_NOTES_ACCESS_TOKEN@git.var-lab.com/opencal/documentation.git documentation
+cd documentation
+git checkout -b "add_changelog_$CI_COMMIT_TAG"
+mkdir -p docs/changelog/backend/
+mv ../release_notes.md "docs/changelog/backend/$CI_COMMIT_TAG.md"
+git add "docs/changelog/backend/$CI_COMMIT_TAG.md"
+git commit -m "add changelog file docs/changelog/backend/$CI_COMMIT_TAG.md"
+git push -u origin "add_changelog_$CI_COMMIT_TAG"
