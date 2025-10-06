adf4c9da654e61ccc171325876299585d9df8631
Anton Dachauer <ad@var-lab.com>
2025-10-06 13:12:17 +0200

    fix automated changelog push
    Changelog: fix

--- ci/push_changelog.sh
+++ ci/push_changelog.sh
@@ -23,22 +23,22 @@ cd documentation
 git fetch origin
 
 if git show-ref --verify --quiet refs/heads/add_changelog_$CI_COMMIT_TAG; then
-  git branch -D add_changelog_$CI_COMMIT_TAG
+  git branch -D add_changelog_backend_$CI_COMMIT_TAG
 fi
-git checkout -b "add_changelog_$CI_COMMIT_TAG"
+git checkout -b "add_changelog_backend_$CI_COMMIT_TAG"
 
 mkdir -p docs/changelog/backend/
 cp ../release_notes.md "docs/changelog/backend/$CI_COMMIT_TAG.md"
 git add "docs/changelog/backend/$CI_COMMIT_TAG.md"
 git commit -m "add changelog file docs/changelog/backend/$CI_COMMIT_TAG.md"
-git push -u origin "add_changelog_$CI_COMMIT_TAG"
+git push -u origin "add_changelog_backend_$CI_COMMIT_TAG"
 
 curl -sS -v \
   -X POST \
   -H "PRIVATE-TOKEN: $OPENCAL_PUSH_RELEASE_NOTES_ACCESS_TOKEN" \
   -H "Content-Type: application/json" \
   -d "{
-    \"source_branch\": \"add_changelog_$CI_COMMIT_TAG\",
+    \"source_branch\": \"add_changelog_backend_$CI_COMMIT_TAG\",
     \"target_branch\": \"main\",
     \"title\": \"Changelog backend $CI_COMMIT_TAG\"
   }" \
