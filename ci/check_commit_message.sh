6f2ee5ec9fe0a69ee8822a180322aac1d0a815ce
Anton Dachauer <ad@var-lab.com>
2025-10-07 20:50:32 +0200

    fix commit message check
    Changelog: chore

--- ci/check_commit_message.sh
+++ ci/check_commit_message.sh
@@ -11,21 +11,21 @@
 # Visit https://git.var-lab.com/opencal/backend/-/blob/main/LICENSE to read the full license text.
 #
 
+set -xe
+
 echo "Checking commit messages..."
 
 VALID_TYPES="feat|fix|docs|chore"
 INVALID_MESSAGES=0
 
-for COMMIT in $(git rev-list origin/main..HEAD); do
-  MESSAGE=$(git log --format=%B -n 1 "$COMMIT")
-  echo "Checking commit: $COMMIT"
-  echo "$MESSAGE" | grep -qE "Changelog: ($VALID_TYPES)$"
-  if [ $? -ne 0 ]; then
-    echo "❌ Invalid commit message in $COMMIT:"
-    echo "$MESSAGE"
-    INVALID_MESSAGES=1
-  fi
-done
+MESSAGE=$(git log --format=%B -n 1 | cat)
+echo "Checking commit: $COMMIT"
+echo "$MESSAGE" | grep -qE "Changelog: ($VALID_TYPES)$"
+if [ $? -ne 0 ]; then
+  echo "❌ Invalid commit message in $COMMIT:"
+  echo "$MESSAGE"
+  INVALID_MESSAGES=1
+fi
 
 if [ "$INVALID_MESSAGES" -ne 0 ]; then
   echo "❌ the commit message must contain the changelog type (Changelog: <type>). Valid types are: feat, fix, docs, chore. Please edit the commit message."
